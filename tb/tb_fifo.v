30df93372bd6b65a9130058ebfedd424083e84c0
Jay Convertino <johnathan.convertino.1@us.af.mil>
2023-02-27 14:25:31 -0500

    first version of fifo with file test, works but is a bit of a hack for the last word indicator. will only work in the current case. needs work.

--- tb/tb_fifo.v
+++ tb/tb_fifo.v
@@ -28,13 +28,15 @@
 
 `timescale 1 ns/10 ps
 
-module tb_fifo;
+module tb_fifo #(
+  parameter IN_FILE_NAME = in.bin,
+  parameter OUT_FILE_NAME = out.bin,
+  parameter FIFO_DEPTH = 64,
+  parameter RAND_FULL = 0
+  )();
   //parameter or local param bus, user and dest width? and files as well? 
   
-  localparam BYTE_WIDTH = 14;
-  
-  localparam CLK_PERIOD = 500;
-  localparam RST_PERIOD = 1000;
+  localparam BYTE_WIDTH = 8;
   
   wire                      tb_stim_clk;
   wire                      tb_stim_rstn;
@@ -42,30 +44,31 @@ module tb_fifo;
   wire [(BYTE_WIDTH*8)-1:0] tb_stim_data;
   wire                      tb_stim_empty;
   wire                      tb_stim_ready;
+  wire                      tb_dut_clk;
+  wire                      tb_dut_rstn;
   wire [(BYTE_WIDTH*8)-1:0] tb_dut_data;
   wire                      tb_dut_valid;
+  wire                      tb_dut_empty;
   wire                      tb_dut_ready;
-  
-  integer num_read = 0;
-  
+
   clk_stimulus #(
-    .CLOCKS(1), // # of clocks
+    .CLOCKS(2), // # of clocks
     .CLOCK_BASE(1000000), // clock time base mhz
     .CLOCK_INC(1000), // clock time diff mhz
-    .RESETS(1), // # of resets
+    .RESETS(2), // # of resets
     .RESET_BASE(2000), // time to stay in reset
     .RESET_INC(100) // time diff for other resets
   ) clk_stim (
     //clk out ... maybe a vector of clks with diff speeds.
-    .clkv(tb_stim_clk),
+    .clkv({tb_dut_clk, tb_stim_clk}),
     //rstn out ... maybe a vector of rsts with different off times
-    .rstnv(tb_stim_rstn),
+    .rstnv({tb_dut_rstn, tb_stim_rstn}),
     .rstv()
   );
   
   write_fifo_stimulus #(
     .BYTE_WIDTH(BYTE_WIDTH),
-    .FILE("in.bin")
+    .FILE(IN_FILE_NAME)
   ) write_fifo_stim (
     .rd_clk(tb_stim_clk),
     .rd_rstn(tb_stim_rstn),
@@ -77,7 +80,7 @@ module tb_fifo;
 
   // FIFO that emulates Xilinx FIFO.
   fifo #(
-    .FIFO_DEPTH(256),
+    .FIFO_DEPTH(FIFO_DEPTH),
     .BYTE_WIDTH(BYTE_WIDTH),
     .COUNT_WIDTH(8),
     .FWFT(0),
@@ -99,12 +102,12 @@ module tb_fifo;
     .wr_data(tb_stim_data),
     .wr_full(tb_stim_ready),
     // read interface
-    .rd_clk(tb_stim_clk),
-    .rd_rstn(tb_stim_rstn),
+    .rd_clk(tb_dut_clk),
+    .rd_rstn(tb_dut_rstn),
     .rd_en(~tb_dut_ready),
     .rd_valid(tb_dut_valid),
     .rd_data(tb_dut_data),
-    .rd_empty(),
+    .rd_empty(tb_dut_empty),
     // data count interface
     .data_count_clk(tb_stim_clk),
     .data_count_rstn(tb_stim_rstn),
@@ -113,15 +116,23 @@ module tb_fifo;
   
   read_fifo_stimulus #(
     .BYTE_WIDTH(BYTE_WIDTH),
-    .FILE("out.bin")
+    .RAND_FULL(RAND_FULL),
+    .FILE(OUT_FILE_NAME)
   ) read_fifo_stim (
-    .wr_clk(tb_stim_clk),
-    .wr_rstn(tb_stim_rstn),
+    .wr_clk(tb_dut_clk),
+    .wr_rstn(tb_dut_rstn),
     .wr_en(tb_dut_valid),
     .wr_ack(),
     .wr_data(tb_dut_data),
     .wr_full(tb_dut_ready)
   );
+  
+  // this is crap, that works for now
+  always @(posedge tb_dut_empty) begin
+    if(tb_dut_empty & tb_stim_empty & !tb_dut_valid & tb_stim_rstn & tb_dut_rstn) begin
+      $finish();
+    end
+  end
   
   // vcd dump command
   initial begin
